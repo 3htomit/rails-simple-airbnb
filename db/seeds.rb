404e4d4aceb6c2edafc176f9b7f428eeacaf3976
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-03-21 13:59:26 +0100

    Created Flat model, routes and controller + Created seeds for it

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+4.times do
+  Flat.create(
+    name: Faker::Book.title,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(50..200),
+    number_of_guests: rand(5..15)
+  )
+end
